cec481c362750d7c9f455c205e15a2301433db9b
unknown <mayedakonika@gmail.com>
2022-11-21 13:10:25 +0600

    header added and favicon also website name

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header/Header';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Main from './components/Main/Main';
+import Home from './components/Home/Home';
+import About from './components/About/About';
+import Statictis from './components/Statictis/Statictis';
+import Blog from './components/Blog/Blog';
 
 function App() {
+  const router=createBrowserRouter([
+    {path:'/',element:<Main></Main>,
+  children:[
+    {path:'/',element:<Home></Home>},
+    {path:'about',element:<About></About>},
+    {path:'statictis',element:<Statictis></Statictis>},
+    {path:'blog',element:<Blog></Blog>}
+  ]}
+  ])
   return (
-    <div className="App">
-      
-      
+    <div >
+
+      <RouterProvider router={router}></RouterProvider>
+
     </div>
   );
 }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Nav } from "react-bootstrap";
+import { Link, NavLink } from "react-router-dom";
+
+const Header = () => {
+    let activeStyle = {
+        backgroundColor: '#7CB342',
+      };
+  return (
+   <div className=" shadow-lg h-50">
+     <Nav  className="d-flex align-items-center container justify-content-between " >
+        <div >
+            <h4>Programming Quizz </h4>
+        </div>
+        <div className=" " >
+        <NavLink to={'/'}  
+            className="m-2 text-decoration-none fs-5" >Home</NavLink>
+        <NavLink to={'/about'} className="m-2 text-decoration-none fs-5">About</NavLink>
+        <NavLink to={'/statictis'} className="m-2 text-decoration-none fs-5">Statictis</NavLink>
+        <NavLink to={'/blog'} className="m-2 text-decoration-none fs-5">Blog</NavLink>
+        </div>
+      </Nav>
+    
+   </div>
+      
+  );
+};
+
+export default Header;
